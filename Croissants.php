7af04bb87799dc01198553d1f47e1a5f310b0323
erzaaaq77 <eq73339@ubt-uni.net>
2026-01-16 16:59:04 +0100

    commit

--- Croissants.php
+++ Croissants.php
@@ -20,9 +20,9 @@
     </div>
     <div class="nav-center" id="nav-links">
         <a href="homee.html">Home</a>
-        <a href="Produktet.html">Products</a>
-        <a href="aboutus.html">About Us</a>
-        <a href="gift.html">Gift Box</a>
+        <a href="Produktet.php">Products</a>
+        <a href="aboutus.php">About Us</a>
+        <a href="gift.php">Gift Box</a>
     </div>
     <div class="nav-right">
           <div class="search-bar">
